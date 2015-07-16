ecb9549dbb86a77feb5d90fc6aa1d0d75ae1577b
Sergey Chernyshev <github.com@antispam.sergeychernyshev.com>
2015-07-16 05:19:36 +0000

    Using domain-hashed folder structure and content hash for file name for result files

--- step4.pl
+++ step4.pl
@@ -8,6 +8,7 @@ use WWW::Favicon;
 use JSON;
 use Digest::MD5;
 use Getopt::Long qw(GetOptions);
+use Digest::MD5 qw(md5_hex);
  
 my $temp_folder;
 GetOptions('folder=s' => \$temp_folder) or die "Must provide folder name for storing icons";
@@ -17,9 +18,14 @@ while(<>) {
 	s/,$//;
 	my $icon = from_json($_);
 
+	my $skip = !exists($$icon{"changed"}) || !$$icon{"changed"} || !exists($$icon{"icon_file"});
+
+	# No need for a temporary file name in results set
+	delete $$icon{"icon_file"};
+
 	print to_json($icon) . ",\n";
 
-	next if (!exists($$icon{"changed"}) || !$$icon{"changed"} || !exists($$icon{"icon_file"}));
+	next if $skip;
 
 	my $image = Image::Magick->new;
 	my $x = $image->Read($$icon{"icon_file"});
@@ -29,7 +35,18 @@ while(<>) {
 	{
 		if ($i->Get('width') == 16 && $i->Get('height') == 16)
 		{
-			$x = $i->Write("$temp_folder/" . $$icon{"domain"} . ".png");
+			my $filename = $$icon{"domain"} . '-' . $$icon{"hash"} . ".png"
+			my $domain_hash = md5_hex($$icon{"domain"});
+			my $folder = $temp_folder . '/' . substr($domain_hash, 0, 2) . '/' . substr($domain_hash, 2, 2);
+
+			mkdir $temp_folder . '/' . substr($domain_hash, 0, 2);
+			mkdir $temp_folder . '/' . substr($domain_hash, 0, 2) . '/' . substr($domain_hash, 2, 2);
+
+			$x = $i->Write($temp_folder . '/' .
+				substr($domain_hash, 0, 2) . '/' .
+				substr($domain_hash, 2, 2) . '/' .
+				$filename);
+
 			warn $x if $x;
 			last;
 		}
